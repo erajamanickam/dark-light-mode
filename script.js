7a1b3a81bc258f6da9cc88112c6f113779c20618
Rajamanickam <rajamanickam.e@xerago.com>
2023-03-18 21:03:28 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var body = document.querySelector('body');
+
+document.getElementById('checkbox').addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    localStorage.setItem("mode", "dark");
+
+    if (!body.classList.contains("dark")) {
+        return localStorage.setItem("mode", "light")
+    }
+})
+
+if (localStorage.getItem("mode") === "dark") {
+    body.classList.toggle('dark');
+}
+
